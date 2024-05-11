cba8342398e375ac9cbe7a0ee15379effc107b46
Rafael Ban <banrafael01@gmail.com>
2024-05-12 02:12:17 +0300

    try fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="h-full w-full">
-      <ToastContainer toastStyle={{ backgroundColor: "#1d1e27" }} />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
